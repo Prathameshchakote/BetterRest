a9b051b5b36abcd27df812093771a60b409ebd43
Prathamesh Chakote <30796045+Prathameshchakote@users.noreply.github.com>
2024-03-13 08:53:58 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Prathamesh on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
